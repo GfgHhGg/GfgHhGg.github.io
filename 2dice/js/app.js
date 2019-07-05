d673493f36896998e8e3cefb152ce27200813a01
Gfghhgg <tish.workmail@gmail.com>
2019-07-05 09:47:45 +0700

    center section added

--- 2dice/js/app.js
+++ 2dice/js/app.js
@@ -5,3 +5,14 @@ $('.chat__tab').click(function () {
   $('.chat__tab_active').removeClass('chat__tab_active');
   $(this).addClass('chat__tab_active');
 });
+$('.counter').click(function (e) {
+  var $self = $(this);
+  var $trg = $(e.target);
+  var sum = $self.find('.counter__total').html();
+
+  if ($trg.hasClass('counter__btn-pls')) {
+    $self.find('.counter__total').html(+sum + 100);
+  } else if ($trg.hasClass('counter__btn-min') && sum > 100) {
+    $self.find('.counter__total').html(sum - 100);
+  }
+});
