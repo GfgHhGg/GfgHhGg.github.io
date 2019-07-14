52deab5ed493b90ce8cf6e0ed67f820e9a84f6ed
Gfghhgg <tish.workmail@gmail.com>
2019-07-14 17:23:03 +0700

    last commit

--- Swapika/js/app.js
+++ Swapika/js/app.js
@@ -1,1 +1,46 @@
 "use strict";
+
+$(document).ready(function () {
+  $('.reviews-slider').slick({
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    variableWidth: true
+  });
+  $('.news-slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+  $('.arrow-abs').on('click', function () {
+    $(this).parent().find('.currency-wrp').toggleClass('show');
+    $(this).toggleClass('to-top');
+  });
+  $('.burger-opener').on('click', function () {
+    $('.menu').toggleClass('show');
+    $('.burger-opener').toggleClass('show');
+  }); // currency-tab
+
+  $('.currency_give').first().addClass('active-give');
+  $('.currency_give').on('click', function () {
+    $('.currency_give').removeClass('active-give');
+    $(this).addClass('active-give');
+  }); // currency-tab
+
+  $('.currency_take').first().addClass('active-take');
+  $('.currency_take').on('click', function () {
+    $('.currency_take').removeClass('active-take');
+    $(this).addClass('active-take');
+  });
+});
