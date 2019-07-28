806a5c4ade8068a7c7f0eff75fc66b9831040fc9
Gfghhgg <tish.workmail@gmail.com>
2019-07-28 17:55:05 +0700

    desktop 90%

--- A4l/js/app.js
+++ A4l/js/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+$('.open-modal').click(function () {
+  $('#modal-' + $(this).attr('data-modal')).show();
+});
+$('.modal-exit').click(function (e) {
+  e.stopPropagation();
+
+  if (e.target == this) {
+    $('.modal').hide();
+  }
+});
