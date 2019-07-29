174bbeb158746e535934ae271ece0390af06b527
Gfghhgg <tish.workmail@gmail.com>
2019-07-30 01:02:10 +0700

    adaptive 80%

--- A4l/js/app.js
+++ A4l/js/app.js
@@ -10,3 +10,17 @@ $('.modal-exit').click(function (e) {
     $('.modal').hide();
   }
 });
+var burgerOpen = false;
+$('.burger-wrp').click(function (e) {
+  var $menu = $('.burger-menu');
+
+  if (burgerOpen) {
+    burgerOpen = false;
+    $($(this).children()[0]).removeClass('icomoon', 'ico-exit').addClass('burger');
+    $menu.css('right', 'calc(100% + 40vw)');
+  } else {
+    burgerOpen = true;
+    $($(this).children()[0]).removeClass('burger').addClass('icomoon ico-exit');
+    $menu.css('right', 'calc(100% - 275px)');
+  }
+});
