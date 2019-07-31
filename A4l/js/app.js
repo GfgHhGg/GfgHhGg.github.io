60ab548bc51ef2e0dd03a9e73810903c6f4b98e3
Gfghhgg <tish.workmail@gmail.com>
2019-07-31 20:37:41 +0300

    v1.3 another edits

--- A4l/js/app.js
+++ A4l/js/app.js
@@ -1,29 +1,47 @@
 "use strict";
 
-$('.open-modal').click(function () {
-  $('#modal-' + $(this).attr('data-modal')).show();
-});
-$('.modal-exit').click(function (e) {
-  e.stopPropagation();
+$(document).ready(function () {
+  var appSpecChange = false;
 
-  if (e.target == this) {
-    $('.modal').hide();
+  if ($(window).width() <= 800) {
+    appSpecChange = true;
+    $('.app-spec__right').appendTo($('.app-spec'));
   }
-});
-var burgerOpen = false;
-$('.burger-wrp').click(function (e) {
-  var $menu = $('.burger-menu');
 
-  if (burgerOpen) {
-    burgerOpen = false;
-    $($(this).children()[0]).removeClass('icomoon', 'ico-exit').addClass('burger');
-    $menu.css('right', 'calc(100% + 40vw)');
-  } else {
-    burgerOpen = true;
-    $($(this).children()[0]).removeClass('burger').addClass('icomoon ico-exit');
-    $menu.css('right', 'calc(100% - 275px)');
-  }
-});
-$('.lang-select-wrp').click(function () {
-  $(this).toggleClass('lang-select-open');
+  $(window).resize(function () {
+    if ($(window).width() <= 800 && !appSpecChange) {
+      appSpecChange = true;
+      $('.app-spec__right').appendTo($('.app-spec'));
+    } else if ($(window).width() > 800 && appSpecChange) {
+      appSpecChange = false;
+      $('.app-spec__right').appendTo($('.app-spec__top'));
+    }
+  });
+  $('.open-modal').click(function () {
+    $('#modal-' + $(this).attr('data-modal')).show();
+  });
+  $('.modal-exit').click(function (e) {
+    e.stopPropagation();
+
+    if (e.target == this) {
+      $('.modal').hide();
+    }
+  });
+  var burgerOpen = false;
+  $('.burger-wrp').click(function (e) {
+    var $menu = $('.burger-menu');
+
+    if (burgerOpen) {
+      burgerOpen = false;
+      $($(this).children()[0]).removeClass('icomoon', 'ico-exit').addClass('burger');
+      $menu.css('right', 'calc(100% + 40vw)');
+    } else {
+      burgerOpen = true;
+      $($(this).children()[0]).removeClass('burger').addClass('icomoon ico-exit');
+      $menu.css('right', 'calc(100% - 275px)');
+    }
+  });
+  $('.lang-select-wrp').click(function () {
+    $(this).toggleClass('lang-select-open');
+  });
 });
